8b36363f2171ca3e7c10fb779d94ed847b6402ff
Macil <Macil.dev@gmail.com>
2015-03-09 08:59:18 +0300

    add domain model

--- src/Data/P440/Domain/Model/Types.hs
+++ src/Data/P440/Domain/Model/Types.hs
@@ -1,4 +1,9 @@
+{-#LANGUAGE DeriveDataTypeable#-}
 module Data.P440.Domain.Model.Types where
+
+import qualified Data.P440.Domain.ComplexTypes as C
+
+import Data.Typeable (Typeable)
 
 import Control.Lens
 
@@ -8,3 +13,15 @@ class ЭлектронныйДокумент a where
     идДокумента   :: Lens' a Text
     датаДокумента :: Lens' a Text
     датаОперации  :: Lens' a Text
+
+data Налогоплательщик =
+      НалогоплательщикЮЛ C.ПлЮЛ
+    | НалогоплательщикИП C.ПлФЛ
+    | НалогоплательщикФЛ C.ПлФЛ
+    deriving (Eq, Show, Typeable)
+
+class ЕстьНалогоплательщик a where
+    налогоплательшик :: Getter a Налогоплательщик
+
+class ЕстьНаименование a where
+    наименование :: Getter a Text
